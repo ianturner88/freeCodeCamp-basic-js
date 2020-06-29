3586530ead922694e9624c8550bef4c387016dd6
Ian Turner <ianturner88@gmail.com>
2020-06-29 13:18:16 -0400

    Fifth lesson complete

---
+++ L5-uninitialized-variables.js
@@ -0,0 +1,11 @@
+// initially js variables have a value of undefined
+
+/* if you do a math operation on an undefined variable,
+the result is NaN -> Not a number */
+
+/* if you concatenate a string with an undefined variable,
+you get a literal string of 'undefined' */
+
+var a = 5;
+var b = 10;
+var c = "I am a";

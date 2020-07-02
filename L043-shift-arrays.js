9c5a7b2ace8717c2ad32ade1a6bea05e44b9de8c
Ian Turner <ianturner88@gmail.com>
2020-07-01 21:01:38 -0400

    Lesson 50

---
+++ L043-shift-arrays.js
@@ -0,0 +1,7 @@
+// shift() removes the first element of an array
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+var removedFromMyArray = myArray.shift();
---
+++ L047-global-scope.js
@@ -0,0 +1,5 @@
+var myGlobal = 10;
+
+function fun1() {
+    oopsGlobal = 5;
+}

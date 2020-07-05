3a933fa070ad6621633594954daa4a38eb0fc88e
Ian Turner <ianturner88@gmail.com>
2020-07-05 09:57:08 -0400

    1 hr sprint over

---
+++ L090-manipulating-complex-objects.js
@@ -0,0 +1,25 @@
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+        "artist": "Blly Jel",
+        "title": "Pian Man",
+        "release_year": 1974,
+        "formats": [
+          "CD",
+          "8T",
+          "LP"
+        ],
+      }
+    // Add a record here
+  ];
+  

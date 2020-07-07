b22cc84a66aa8a8b1e3aa60d3af09a1de00c10aa
Ian Turner <ianturner88@gmail.com>
2020-07-07 09:59:04 -0400

    Lesson 99 complete

---
+++ L099-nesting-for-loops.js
@@ -0,0 +1,10 @@
+function multiplyAll(arr) {
+    var product = 1;
+    // Only change code below this line
+  
+    // Only change code above this line
+    return product;
+  }
+  
+  multiplyAll([[1,2],[3,4],[5,6,7]]);
+  
